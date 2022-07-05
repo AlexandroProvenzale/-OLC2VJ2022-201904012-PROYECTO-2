60ec49b1d64abac2ebdcb4944d1386def52634ca
AlexandroProvenzale <3056350180301@ingenieria.usac.edu.gt>
2022-07-04 19:17:32 -0600

    Modificación del theme2

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]
+
+primaryColor=\"#6eb52f\"
+backgroundColor=\"#f0f0f5\"
+secondaryBackgroundColor=\"#e0e0ef\"
+textColor=\"#262730\"
+font="sans serif"
 " > ~/.streamlit/config.toml
